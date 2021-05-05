e888833daa39d1a9eb2973da5496aa80a5ca46b9
Nikolay Vinogradov <nikolya0209@mail.ru>
2021-05-05 12:05:05 +0300

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by MacBookPro on 05.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
