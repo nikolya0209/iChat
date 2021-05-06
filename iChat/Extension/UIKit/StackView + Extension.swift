6f59b1bc20a0c651e11d70e7099b3be5ff10d9f7
Nikolay Vinogradov <nikolya0209@mail.ru>
2021-05-06 21:58:15 +0300

    constraints for first display

---
+++ iChat/Extension/UIKit/StackView + Extension.swift
@@ -0,0 +1,16 @@
+//
+//  StackView + Extension.swift
+//  iChat
+//
+//  Created by MacBookPro on 06.05.2021.
+//
+
+import UIKit
+
+extension UIStackView {
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+    }
+}
