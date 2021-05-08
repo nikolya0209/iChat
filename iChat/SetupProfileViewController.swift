e9102bb78b2a622b95d272c26e056714e7b5a37b
Nikolay Vinogradov <nikolya0209@mail.ru>
2021-05-08 18:05:56 +0300

    Setup Profile Screen

--- iChat/SetupProfileViewController.swift
+++ iChat/SetupProfileViewController.swift
@@ -9,7 +9,19 @@ import UIKit
 
 class SetupProfileViewController: UIViewController {
     
-    let fillImageView = AddPhotoView()
+    let welcomeLabel = UILabel(text: "Set up profile!", font: .avenir26())
+    
+    let fullnameLabel = UILabel(text: "Login with")
+    let aboutMeLabel = UILabel(text: "About me")
+    let sexLabel = UILabel(text: "Sex")
+    
+    let fullNameTextField = OneLineTextField(font: .avenir20())
+    let aboutMeTextField = OneLineTextField(font: .avenir20())
+    let segmentedControl = UISegmentedControl(first: "Male", second: "Femail")
+    
+    let goToChatsButton = UIButton(title: "Go to chats!", titleColor: .white, backgroundColor: .buttonDark(), cornerRadius: 4)
+    
+    let fullImageView = AddPhotoView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,14 +36,37 @@ class SetupProfileViewController: UIViewController {
 extension SetupProfileViewController {
     private func setupConstrints() {
         
-        fillImageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(fillImageView)
+        let fullNameStackView = UIStackView(arrangedSubviews: [fullnameLabel, fullNameTextField] , axis: .vertical, spacing: 0)
+        let aboutMeStackView = UIStackView(arrangedSubviews: [aboutMeLabel, aboutMeTextField] , axis: .vertical, spacing: 0)
+        let sexStackView = UIStackView(arrangedSubviews: [sexLabel, segmentedControl] , axis: .vertical, spacing: 12)
         
+        goToChatsButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        let stackView = UIStackView(arrangedSubviews: [fullNameStackView, aboutMeStackView, sexStackView, goToChatsButton] , axis: .vertical, spacing: 40)
+        
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        fullImageView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(welcomeLabel)
+        view.addSubview(fullImageView)
+        view.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            
-            fillImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
-            fillImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            welcomeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 160),
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+           
+        ])
+        
+        NSLayoutConstraint.activate([
+            fullImageView.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 40),
+            fullImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+           
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: fullImageView.bottomAnchor, constant: 40),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
            
         ])
         
