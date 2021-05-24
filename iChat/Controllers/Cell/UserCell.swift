cc729d0d2f548fc47c939efac1959e9e8cade097
Nikolay Vinogradov <nikolya0209@mail.ru>
2021-05-24 09:04:10 +0300

    Users Realtime Updates

--- iChat/Controllers/Cell/UserCell.swift
+++ iChat/Controllers/Cell/UserCell.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import SDWebImage
 
 class UserCell: UICollectionViewCell, SelfconfiguringCell {
     
@@ -34,10 +34,15 @@ class UserCell: UICollectionViewCell, SelfconfiguringCell {
         self.contentView.clipsToBounds = true
     }
     
+    override func prepareForReuse() {
+        userImageView.image = nil
+    }
+    
     func configure<U>(with value: U) where U : Hashable {
         guard let user: MUser = value as? MUser else { return }
-        userImageView.image = UIImage(named: user.avatarStringURL)
         userName.text = user.username
+        guard let url = URL(string: user.avatarStringURL) else { return }
+        userImageView.sd_setImage(with: url, completed: nil)
     }
     
     private func setupConstraints() {
--- iChat/Controllers/PeopleViewController.swift
+++ iChat/Controllers/PeopleViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import FirebaseAuth
-import Firebase
+import FirebaseFirestore
 
 class PeopleViewController: UIViewController {
   
-    //let users = Bundle.main.decode([MUser].self, from: "users.json")
-    let users = [MUser]()
+    
+    var users = [MUser]()
+    private var usersListener: ListenerRegistration?
+    
     var collectionView:  UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, MUser>!
     
@@ -35,6 +37,10 @@ class PeopleViewController: UIViewController {
         title = currentUser.username
     }
     
+    deinit {
+        usersListener?.remove()
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -42,15 +48,24 @@ class PeopleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
         view.backgroundColor = .orange
         setupSearchBar()
         setupCollectionView()
         createDataSource()
-        reloadData(with: nil)
+        
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(signOut))
+        
+        usersListener = ListenerService.shared.userObserve(users: users, completion: { (result) in
+            switch result {
+            
+            case .success(let users):
+                self.users = users
+                self.reloadData(with: nil)
+            case .failure(let error):
+                self.showAlert(with: "Ошибка!", and: error.localizedDescription)
+            }
+        })
        
     }
     
